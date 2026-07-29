9aa1efb26fee46c2601f2991e5af94fe0e1cfe03
aniadadadu <anadanielanovoo@gmail.com>
2026-07-29 12:01:01 +0100

    first commit. ficheiro SQL para MYSQL

---
+++ project_sql.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE ProjetoFinalIefp;
+USE DATABASE ProjetoFinalIefp;
+
+CREATE TABLE utilizadores(
+id INT AUTO_INCREMENT PRIMARY KEY,
+username VARCHAR(50),
+email VARCHAR(50),
+password CHAR(50),
+);
+
+CREATE TABLE modulos(
+id iNT AUTO_INCREMENT PRIMARY KEY,
+langs VARCHAR(50), -- linguagens de programação
+score INT -- pontuação modulos
+);
+
